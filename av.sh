0022429c13186928b9ce70e1f9d119f9a4aecac2
root <root@TALIS12.localdomain>
2025-01-06 12:25:57 +0100

    add testing pid end all

---
+++ av.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for agr in "$@"
+do
+	echo "$arg"
+done
---
+++ prompt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int command;
+
+	printf("$");
+	scanf("%ls", &command);
+	printf("%d", command);
+
+	return (0);
+}
